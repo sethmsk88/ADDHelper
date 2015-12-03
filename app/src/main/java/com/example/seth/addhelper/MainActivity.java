7f8712e99ec1a8f228bdb8f3aa8258a299134f09
Seth Kerr <sethmsk88@yahoo.com>
2015-12-03 13:35:54 -0500

    Create database

--- app/src/main/java/com/example/seth/addhelper/MainActivity.java
+++ app/src/main/java/com/example/seth/addhelper/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.seth.addhelper;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +22,31 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        SQLiteDatabase myDB = null;
+        String tableName = "tasks";
+        String data = "";
 
+        /* Create a database */
+        try {
+            myDB = this.openOrCreateDatabase("ADHD_DB", MODE_PRIVATE, null);
+
+            /* Create a table in the database */
+            myDB.execSQL("CREATE TABLE IF NOT EXISTS " +
+                    tableName +
+                    " (T_ID INT PRIMARY KEY NOT NULL, " +
+                    " TaskName VARCHAR(64), " +
+                    " LengthMinutes INT, " +
+                    " Days VARCHAR(8))"
+            );
+        }
+        catch (Exception e) {
+            Log.e("Error", "Error", e);
+        }
+        finally {
+            if (myDB != null) {
+                myDB.close();
+            }
+        }
     }
 
     @Override
