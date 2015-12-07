43ae917182d522dc02d32f8513908a5d810c1488
Seth Kerr <sethmsk88@yahoo.com>
2015-12-06 19:41:17 -0500

    Clean up test code

---
+++ app/src/main/java/com/example/seth/addhelper/ViewTasksActivity.java
@@ -0,0 +1,74 @@
+package com.example.seth.addhelper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+public class ViewTasksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_tasks);
+
+        loadTasks();
+
+        /*
+        // TESTING Intents
+        LinearLayout layout = (LinearLayout) findViewById(R.id.linear_layout_0);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(AddTasksActivity.EXTRA_MESSAGE);
+
+        TextView tv = new TextView(this);
+        tv.setTextSize(40);
+        tv.setText(message);
+        layout.addView(tv);*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void changeActivity(View view) {
+        Intent intent = new Intent(this, AddTasksActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Query tasks from database and load them into a list
+     */
+    public void loadTasks() {
+        String[] fromCols = {DatabaseHandler.TASK_NAME};
+        int[] toViews = {android.R.id.text1};
+
+        DatabaseHandler dbHandler = new DatabaseHandler(this);
+        SimpleCursorAdapter myAdapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_1, dbHandler.getCursor(), fromCols, toViews);
+        ListView listView = (ListView) findViewById(R.id.tasks_list_view);
+        listView.setAdapter(myAdapter);
+    }
+
+}
